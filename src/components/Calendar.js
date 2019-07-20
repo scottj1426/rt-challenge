973b49c9d84a592d378a73807df79ff700d617d1
James Scott <scottj1426@gmail.com>
2019-07-20 11:42:10 -0500

    added days of the week

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -2,11 +2,20 @@ import React, { Component } from 'react';
 import moment from 'moment';
 
 class Calendar extends Component {
+    weekDays = moment.weekdays();
+
     render() {
+        let weekDays = this.weekDays.map(day => {
+            return (
+              <th key={day} className="week-day">
+               {day}
+              </th>
+            );
+         });
         return (
             <div>
                 <div>Calendar</div>
-              
+                    <div>{weekDays}</div>
             </div>
         );
     }
